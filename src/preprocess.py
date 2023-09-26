d8747ae0904434f03a5958a51d59a2969c07ddef
CarlosGolvano <carlosgoolvin@gmail.com>
2023-09-26 14:14:15 +0200

    Docs

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,42 +3,45 @@ This python file contains the necessary function to convert annotated data in Br
 
 The files need to be in a specific structure. The directory dataset must contain at least one set (train, dev, test).
 Each set directory must contain two main directories:
+
     1. First one with the text files (raw data)
+
     2. Second one with the file with the annotation. Only one file with all the annotations. The structure of this file
        is as follows (each column is sep by a tab):
 
-            filename    mark  label   off0    off1    span
-            ----------------------------------------------------------
-            fl_example  T1	  HUMAN   112     118	  hombre
-            fl_example  T2	  HUMAN   1025    1033	  paciente
+            .. csv-table::
+                :header: filename, mark, label, off0, off1, span
 
-Example of dataset organization:
+                fl_example, T1, HUMAN, 112, 118, hombre
+                fl_example, T2, HUMAN, 1025, 1033, paciente
 
-/path/to/dataset
-└── cantemist
-    ├── train-set
-    │   ├── cantemist-ner
-    │   │   └── ner_annotations.tsv
-    │   └── text-files
-    │       ├── text-file_1.txt
-    │       ├── text-file_2.txt
-    │       └── ...
-    │
-    ├── dev-set
-    │   ├── cantemist-ner
-    │   │   └── ner_annotations.tsv
-    │   └── text-files
-    │       ├── text-file_1.txt
-    │       ├── text-file_2.txt
-    │       └── ...
-    │
-    └── test-set
-        ├── cantemist-ner
-        │   └── ner_annotations.tsv
-        └── text-files
-            ├── text-file_1.txt
-            ├── text-file_2.txt
-            └── ...
+Example of dataset organization::
+
+    /path/to/dataset
+    └── cantemist
+        ├── train-set
+        │   ├── cantemist-ner
+        │   │   └── ner_annotations.tsv
+        │   └── text-files
+        │       ├── text-file_1.txt
+        │       ├── text-file_2.txt
+        │       └── ...
+        │
+        ├── dev-set
+        │   ├── cantemist-ner
+        │   │   └── ner_annotations.tsv
+        │   └── text-files
+        │       ├── text-file_1.txt
+        │       ├── text-file_2.txt
+        │       └── ...
+        │
+        └── test-set
+            ├── cantemist-ner
+            │   └── ner_annotations.tsv
+            └── text-files
+                ├── text-file_1.txt
+                ├── text-file_2.txt
+                └── ...
 
 Each set has a directory 'text-files' with the raw data and a directory 'cantemist-ner' where the annotated data is
 located
@@ -264,6 +267,7 @@ def process_file(path_txt, df_tsv, path_save, ann_labels, txt_tasks):
 
     This function depends on the organization of the files, and it may be modified.
     In this case, the files are organized as follows:
+
         unique_label_file.tsv
 
         text_file1.txt
